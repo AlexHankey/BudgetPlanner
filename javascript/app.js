d32b58fe15ad41a79432bcaba3fc10e276cc3502
Alex Hankey <Alexhankey1998@hotmail.co.uk>
2023-05-04 22:58:17 +0100

    Nothing works and i want to quit :/

--- javascript/app.js
+++ javascript/app.js
@@ -8,21 +8,39 @@ function getTime(){
     var time = today.getHours() + " : " + today.getMinutes() + " : " + today.getSeconds();
     document.getElementById("clock").innerHTML = time;
 }
-function getYearlyWageValue(){
-    var value = document.getElementById("yearlyWage_Input").value;
-    document.getElementById("output-yearly").innerHTML = "Annual Salary: " + value;
+
+// Table calculations 
+var v = document.getElementById("yearlyWage_Input").value;
+function getYearlyWageValue(val){
+    document.getElementById("Yearly").innerHTML = val;
 }
-function calculateMonthly_preTax(){
-    var value = document.getElementById("yearlyWage_Input").value / 12;
-    document.getElementById("output-monthly").innerHTML = "Month Salary (Pre-Tax): " + value
+function getMonthlyWageValue(val){
+    var monthlyValue = val / 12;
+    document.getElementById("Monthly").innerHTML = monthlyValue;
+}
+function getWeeklyWageValue(val){
+    var weeklyValue = (val * 12) / 52;
+    document.getElementById("Weekly").innerHTML = weeklyValue;
+}
+function getDailyWageValue(val){
+    var dailyValue = val / 5;
+    document.getElementById("Daily").innerHTML = dailyValue;
+}
+function getHourlyWageValue(val){
+    var hourlyValue = val / 8;
+    document.getElementById("Hourly").innerHTML = hourlyValue;
 }
 
+function displayTableContents(v){
+    var userInputValue = v
+    getYearlyWageValue(userInputValue);
+    getMonthlyWageValue(userInputValue);
+    getWeeklyWageValue(userInputValue);
+    getDailyWageValue(userInputValue);
+    getHourlyWageValue(userInputValue);
+}
 
 getTime();
 getDate();
+displayTableContents(v);
 
-function outputValues(){
-    getYearlyWageValue();
-    calculateMonthly_preTax();
-}
-outputValues();
